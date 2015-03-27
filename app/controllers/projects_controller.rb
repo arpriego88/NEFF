48eec53096ea41b281d36306847a9c93cbcdfe72
cmcollin41 <cmcollin41@gmail.com>
2015-03-27 17:13:31 -0600

    new forms

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,48 @@
 class ProjectsController < ApplicationController
-  def index
-  end
+  before_action :vendor_variable
+
 
   def show
+    @project = @vendor.projects.find(params[:id])
   end
 
   def new
+    @project = @vendor.projects.build
+  end
+
+  def create
+    @project = @vendor.projects.build
+    if @project.save
+      flash[:success] = 'You created a new project'
+      redirect_to root_path
+    else
+      flash[:danger] = 'Sorry, something went wrong =/'
+      redirect_to new_vendor_project_path(@project)
+    end
   end
 
   def edit
+    @project = @vendor.projects.find([:id])
+  end
+
+  def update
+    @project = @vendor.projects.find([:id])
+    if @project.update_attributes(project_params)
+      flash[:success] = 'You updated your project'
+      redirect_to root_path
+    else
+      flash[:danger] = 'Sorry, something went wrong'
+      redirect_to edit_vendor_project_path(@project)
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :due_time)
+  end
+
+  def vendor_variable
+    @vendor = Vendor.find(params[:vendor_id])
   end
 end
