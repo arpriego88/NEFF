781f4ad6f5142360c4729bdff04313ed7f8fa0ce
rayrowe125 <gonzai125@gmail.com>
2015-03-27 17:11:19 -0600

    vendor controller; vendor form created; vendor new, index and edit views made

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,13 +1,50 @@
 class VendorsController < ApplicationController
   def index
+    @vendors = Vendor.all 
   end
 
   def show
+    @projects = @vendor.projects.all
   end
 
   def new
+    @vendor = Vendor.new 
+  end
+
+  def create
+    @vendor = Vendor.new(vendor_params)
+    if @vendor.save
+      flash[:success] = "Vendor Created!"
+      redirect_to root_path 
+    else
+      flash[:danger] = "Please fill in all fields"
+      render :new
+    end
   end
 
   def edit
+    @vendor = Vendor.find(params[:id])
+  end
+
+  def update
+    @vendor = Vendor.find(params[:id])
+      if @vendor.update(vendor_params)
+        flash[:success] = "Vendor Updated"
+        redirect_to root_path 
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    @vendor.destroy 
+    flash[:success] = "Deleted '#{@vendor.name}'"
+    redirect_to root_path
+  end
+
+  private
+
+  def vendor_params
+    params.require(:vendor).permit(:name)
   end
 end
